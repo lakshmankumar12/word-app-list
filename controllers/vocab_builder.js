8d27fd87ce7dd68a7c225c447a85f27bba819793
lnara002 <Lakshman_Kumar.Narayanan@alcatel-lucent.com>
2015-09-04 13:02:40 -0700

    wip .. page layout

--- controllers/vocab_builder.js
+++ controllers/vocab_builder.js
@@ -1,19 +1,14 @@
-var model = {
-    question: "Adam",
-    items: [{ action: "Buy Flowers", done: false },
-                { action: "Get Shoes", done: false },
-                { action: "Collect Tickets", done: true },
-                { action: "Call Joe", done: false }]
-};
-
 var todoApp = angular.module("vocab_builder");
+
+var current_result = {
+   'question' : "Question",
+   'choice1'  : "Choice1",
+   'choice2'  : "Choice2",
+   'choice3'  : "Choice3",
+   'choice4'  : "Choice4"
+};
 
 todoApp.constant("dataUrl", "http://localhost:2403/word-list")
   .controller("vocab_builder_controller", function ($scope) {
-    $scope.current_result.question = "Question";
-    $scope.current_result.choice1  = "Choice1";
-    $scope.current_result.choice2  = "Choice2";
-    $scope.current_result.choice3  = "Choice3";
-    $scope.current_result.choice4  = "Choice4";
-
+    $scope.current_result = current_result;
 });
