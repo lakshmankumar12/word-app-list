c7aa6913670bb16dd397935540d377cb0e9fdb61
lnara002 <Lakshman_Kumar.Narayanan@alcatel-lucent.com>
2015-09-04 17:05:26 -0700

    show/hide answered stuff .. and bring right answer into choices

--- controllers/vocab_builder.js
+++ controllers/vocab_builder.js
@@ -5,7 +5,8 @@ var current_result = {
    'choice1'  : "Choice1",
    'choice2'  : "Choice2",
    'choice3'  : "Choice3",
-   'choice4'  : "Choice4"
+   'choice4'  : "Choice4",
+   'choice5'  : "Choice5"
 };
 
 var totalQuestions = 5000;
@@ -28,23 +29,53 @@ function getAlternateWords(word) {
   return result
 }
 
-todoApp.constant("dataUrl", "http://localhost:2403/word-list")
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+todoApp.constant("dataUrl", "word-list")
   .controller("vocab_builder_controller", function ($scope) {
     $scope.current_result = current_result;
 
+    var totalAsked = 0;
+    var totalRight = 0;
+    var rightAnswer = 0;
+
+    $scope.answered = 0;
+    $scope.answer_result = "your answer is being checked";
+
     $scope.getNextQuestion = function () {
-       var question_index =Math.floor(Math.random() * unAsked.length);
+       var question_index = Math.floor(Math.random() * unAsked.length);
        var question = unAsked[question_index];
        unAsked.splice(question_index,1);
 
        var choices = getAlternateWords(question);
+       choices.push(question);
+       shuffle(choices);
+       rightAnswer = choices.indexOf(question);
 
        var nextQuestion = {
           'question' : question,
           'choice1'  : choices[0],
           'choice2'  : choices[1],
           'choice3'  : choices[2],
-          'choice4'  : choices[3]
+          'choice4'  : choices[3],
+          'choice5'  : choices[4]
        }
 
        $scope.current_result = nextQuestion;
